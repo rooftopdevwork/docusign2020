2a4e35967803483e7449aff619cd1dd4ae189dd8
mariomui <yi.kan.mui@gmail.com>
2020-02-16 11:44:08 -0800

    Oauth updates

--- embeddedSigning.js
+++ embeddedSigning.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 
 //On execution an envelope is sent to the provided email address, one signHere
 //tab is added, the document supplied in workingdirectory\fileName is used.
-//Open a new browser pointed at http://localhost:3000 to execute. 
+//Open a new browser pointed at http://localhost:3000 to execute.
 //-------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------
 
@@ -19,11 +19,12 @@ const fs = require('fs');
 //Obtain your accountId from account-d.docusign.com > Go To Admin > API and Keys
 
 // const OAuthToken = 'eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwAAsZV-35nWSAgAAPG4jCKa1kgCAGBu6J-HX45FpTYdVXSE3uUVAAEAAAAYAAEAAAAFAAAADQAkAAAAZjBmMjdmMGUtODU3ZC00YTcxLWE0ZGEtMzJjZWNhZTNhOTc4EgABAAAACwAAAGludGVyYWN0aXZlMACAGv1935nWSDcA-_hnj_5Lm0-SAhEQzwcUyg.Q8UQZUxai2WxFmoN8vJBi9cxmUUgE1Eg8EPM-P4EMzY6johjaq7fYmUVRFHnq731mQqh8URduzWBz4uJP2blL-WJJzkPYRNYKOEcs4U1sIPmqZ1Tk3vW19wPbow2RbdpBHaFA9zyL1u-c1fBDqTzRzkcG9herOzM9czE6pBxQqcA9M3NZDNbUrxxc6Q5qBWhUEmouIMEXtdMHT9SknAbmFRZH_rFlenHqRHCq2j4qTB8u4hQTzp6NpGfQWIZ1iXOpYE4CAA22CVNzk3cRdjJtm8Ia_pUJy1PLjmi7gC986xAj64Wel1lmpDsJ0dVbp7Am6SUXJCAFEgQjejEbhrS3g';
-const OAuthToken = 'eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwAAJ5Dh9OzWSAgAAGez7zft1kgCAGBu6J-HX45FpTYdVXSE3uUVAAEAAAAYAAkAAAAFAAAAKwAAAC0AAAAvAAAAMQAAADIAAAA4AAAAMwAAADUAAAANACQAAABmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgSAAEAAAALAAAAaW50ZXJhY3RpdmUwAAD6XuD07NZINwDnlbHeHXOAS7fUyFvGfQNF.zuVkC30oi8qd9E-_g5j0I8KbxETs756FTlHu3rPVvt2Z3j5nmWe4Z75DWnLHw7oEUAKBtHqPf8mEIyCDbf9tFIghNUnbP6JbEsD4XhUcFdP7FLQjVTDv2H083cPxOvnc1alCZ5sdPkBDzRdCP3j_BHXfWBxRNY47YKNDUrCl8kbj5Kt9Y1x74E8kSHKCo_7_tORKuKGJvreexlqw0E4U8YkYYTxRvyW9_a0hX40IGrj9lODCbStHfjP-7ZPGFZnK-VBSyksJjHqF9R4iGaBDk3UJWNzmb5CgTQsHjaCX3eJjtlUs4ctY88oHrcFVB-gSeSMTIRNg30QLlByJyNEOuQ';
+// const OAuthToken = 'eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwAAJ5Dh9OzWSAgAAGez7zft1kgCAGBu6J-HX45FpTYdVXSE3uUVAAEAAAAYAAkAAAAFAAAAKwAAAC0AAAAvAAAAMQAAADIAAAA4AAAAMwAAADUAAAANACQAAABmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgSAAEAAAALAAAAaW50ZXJhY3RpdmUwAAD6XuD07NZINwDnlbHeHXOAS7fUyFvGfQNF.zuVkC30oi8qd9E-_g5j0I8KbxETs756FTlHu3rPVvt2Z3j5nmWe4Z75DWnLHw7oEUAKBtHqPf8mEIyCDbf9tFIghNUnbP6JbEsD4XhUcFdP7FLQjVTDv2H083cPxOvnc1alCZ5sdPkBDzRdCP3j_BHXfWBxRNY47YKNDUrCl8kbj5Kt9Y1x74E8kSHKCo_7_tORKuKGJvreexlqw0E4U8YkYYTxRvyW9_a0hX40IGrj9lODCbStHfjP-7ZPGFZnK-VBSyksJjHqF9R4iGaBDk3UJWNzmb5CgTQsHjaCX3eJjtlUs4ctY88oHrcFVB-gSeSMTIRNg30QLlByJyNEOuQ';
 // const accountId = 'f5afd249-305e-4cc8-a456-d16380991b61';
-const accountId = '9fe86e60-5f87-458e-a536-1d557484dee5';
-
-
+// const accountId = '9fe86e60-5f87-458e-a536-1d557484dee5';
+const OAuthToken =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjY4MTg1ZmYxLTRlNTEtNGNlOS1hZjFjLTY4OTgxMjIwMzMxNyJ9.eyJUb2tlblR5cGUiOjUsIklzc3VlSW5zdGFudCI6MTU4MTgyMzc3OSwiZXhwIjoxNTgxODUyNTc5LCJVc2VySWQiOiI5ZmU4NmU2MC01Zjg3LTQ1OGUtYTUzNi0xZDU1NzQ4NGRlZTUiLCJzaXRlaWQiOjEsInNjcCI6WyJzaWduYXR1cmUiLCJjbGljay5tYW5hZ2UiLCJvcmdhbml6YXRpb25fcmVhZCIsInJvb21fZm9ybXMiLCJncm91cF9yZWFkIiwicGVybWlzc2lvbl9yZWFkIiwidXNlcl9yZWFkIiwidXNlcl93cml0ZSIsImFjY291bnRfcmVhZCIsImRvbWFpbl9yZWFkIiwiaWRlbnRpdHlfcHJvdmlkZXJfcmVhZCIsImR0ci5yb29tcy5yZWFkIiwiZHRyLnJvb21zLndyaXRlIiwiZHRyLmRvY3VtZW50cy5yZWFkIiwiZHRyLmRvY3VtZW50cy53cml0ZSIsImR0ci5wcm9maWxlLnJlYWQiLCJkdHIucHJvZmlsZS53cml0ZSIsImR0ci5jb21wYW55LnJlYWQiLCJkdHIuY29tcGFueS53cml0ZSJdLCJhdWQiOiJmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgiLCJhenAiOiJmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgiLCJpc3MiOiJodHRwczovL2FjY291bnQtZC5kb2N1c2lnbi5jb20vIiwic3ViIjoiOWZlODZlNjAtNWY4Ny00NThlLWE1MzYtMWQ1NTc0ODRkZWU1IiwiYW1yIjpbImludGVyYWN0aXZlIl0sImF1dGhfdGltZSI6MTU4MTgyMzc3OCwicHdpZCI6IjlkYjE1YWI1LWE2ZWMtNDhkNS1hZDM3LTczMGMzZDNjYTViZiJ9.31vrLSfZK3wp_S6Mmz1BCkhHupagI2i5Qpm-fZxMIUcTYve3zyi3Qk-jjtO1j87ZxJtrOdeueRIizeR3R6M5A726kU5VCu0U8c8S0v9AtUytEwSGZ16Ax0VlqhAv9ecTBEJIqxyoHQKmC4kAMhyHMoO2aJ1xisJXBaGc4xOg-GLGHTvekB0A2KJXrpqo-IcZvH16RlnTkG-_Q1jYzldY5cdoUXt9CPpH_j3wjtM8ObKYhhuJ4SK68G9si9kJ0PWoJSBeScDMn4sJyBXWW13S7RZIbOowSgvpHCakSQioc3I0kgQ5pIXvv1URzl9UQbz0KhKy_hdTYapVc3ImuQ6vXw';
+const accountId = 'f5afd249-305e-4cc8-a456-d16380991b61';
 
 //Recipient Information goes here
 const recipientName = 'mario mui';
@@ -35,13 +36,11 @@ const fileName = 'docs/House.pdf';
 //-------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------
 
-app.get('/', function (req, res) {
-
+app.get('/', function(req, res) {
   apiClient.setBasePath('https://demo.docusign.net/restapi');
   apiClient.addDefaultHeader('Authorization', 'Bearer ' + OAuthToken);
 
   // *** Begin envelope creation ***
-
 
   //Read the file you wish to send from the local machine.
   fileStream = process.argv[2];
@@ -54,7 +53,8 @@ app.get('/', function (req, res) {
 
   //Set the Email Subject line and email message
   envDef.emailSubject = 'Please sign this document sent from Node SDK';
-  envDef.emailBlurb = 'Please sign this document sent from the DocuSign Node.JS SDK.'
+  envDef.emailBlurb =
+    'Please sign this document sent from the DocuSign Node.JS SDK.';
 
   //Read the file from the document and convert it to a Base64String
   let doc = new docusign.Document();
@@ -110,56 +110,62 @@ app.get('/', function (req, res) {
   //Then add the recipients object to the enevelope definitions
   envDef.recipients = recipients;
 
-  // *** End envelope creation *** 
-
+  // *** End envelope creation ***
 
   //Send the envelope
   let envelopesApi = new docusign.EnvelopesApi();
-  envelopesApi.createEnvelope(accountId, { 'envelopeDefinition': envDef }, function (err, envelopeSummary, response) {
-
-    if (err) {
-      return res.send('Error while creating a DocuSign envelope:' + err);
-    }
-    //Set envelopeId the envelopeId that was just created
-    let envelopeId = envelopeSummary.envelopeId;
-
-    //Fill out the recipient View request. authenticationMethod should be email. ClientUserId, RecipientId, returnUrl, userName (Full name of the signer), and email are required.
-    //If a clientUserId was not specified, leave it out.
-    let recipientViewRequest = new docusign.RecipientViewRequest();
-    recipientViewRequest.authenticationMethod = 'email';
-    recipientViewRequest.clientUserId = '123';
-    recipientViewRequest.recipientId = '1';
-    recipientViewRequest.returnUrl = 'http://localhost:3000/dsreturn';
-    recipientViewRequest.userName = recipientName;
-    recipientViewRequest.email = recipientEmail;
-
-    //Create the variable used to handle the response
-    recipientViewResults = docusign.ViewLinkRequest();
-
-    //Make the request for a recipient view
-    envelopesApi.createRecipientView(accountId, envelopeId, { recipientViewRequest: recipientViewRequest }, function (err, recipientViewResults, response) {
-
+  envelopesApi.createEnvelope(
+    accountId,
+    { envelopeDefinition: envDef },
+    function(err, envelopeSummary, response) {
       if (err) {
-        return res.send('Error while creating a DocuSign recipient view:' + err);
+        return res.send('Error while creating a DocuSign envelope:' + err);
       }
+      //Set envelopeId the envelopeId that was just created
+      let envelopeId = envelopeSummary.envelopeId;
 
-      //Set the signingUrl variable to the link returned from the CreateRecipientView request
-      let signingUrl = recipientViewResults.url;
+      //Fill out the recipient View request. authenticationMethod should be email. ClientUserId, RecipientId, returnUrl, userName (Full name of the signer), and email are required.
+      //If a clientUserId was not specified, leave it out.
+      let recipientViewRequest = new docusign.RecipientViewRequest();
+      recipientViewRequest.authenticationMethod = 'email';
+      recipientViewRequest.clientUserId = '123';
+      recipientViewRequest.recipientId = '1';
+      recipientViewRequest.returnUrl = 'http://localhost:3000/dsreturn';
+      recipientViewRequest.userName = recipientName;
+      recipientViewRequest.email = recipientEmail;
 
-      //Then redirect to the signing URL
-      res.redirect(signingUrl);
-    });
+      //Create the variable used to handle the response
+      recipientViewResults = docusign.ViewLinkRequest();
 
-  });
+      //Make the request for a recipient view
+      envelopesApi.createRecipientView(
+        accountId,
+        envelopeId,
+        { recipientViewRequest: recipientViewRequest },
+        function(err, recipientViewResults, response) {
+          if (err) {
+            return res.send(
+              'Error while creating a DocuSign recipient view:' + err
+            );
+          }
 
+          //Set the signingUrl variable to the link returned from the CreateRecipientView request
+          let signingUrl = recipientViewResults.url;
+
+          //Then redirect to the signing URL
+          res.redirect(signingUrl);
+        }
+      );
+    }
+  );
 });
 
-app.get('/dsreturn', function (req, res) {
+app.get('/dsreturn', function(req, res) {
   //Enter code here for post-processing after enevelope signing has been completed.
   res.send('Welcome back, enter followup code / processing information here.');
 });
 
-app.listen(port, host, function (err) {
+app.listen(port, host, function(err) {
   if (err) {
     return res.send('Error while starting the server:' + err);
   }
--- sendEnvelope.js
+++ sendEnvelope.js
@@ -7,10 +7,9 @@ const port = process.env.PORT || 3000;
 const host = process.env.HOST || 'localhost';
 const fs = require('fs');
 
-
 //On execution an envelope is sent to the provided email address, one signHere
 //tab is added, the document supplied in workingdirectory\fileName is used.
-//Open a new browser pointed at http://localhost:3000 to execute. 
+//Open a new browser pointed at http://localhost:3000 to execute.
 //-------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------
 
@@ -21,16 +20,17 @@ const fs = require('fs');
 
 // const OAuthToken = 'eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwAAsZV-35nWSAgAAPG4jCKa1kgCAGBu6J-HX45FpTYdVXSE3uUVAAEAAAAYAAEAAAAFAAAADQAkAAAAZjBmMjdmMGUtODU3ZC00YTcxLWE0ZGEtMzJjZWNhZTNhOTc4EgABAAAACwAAAGludGVyYWN0aXZlMACAGv1935nWSDcA-_hnj_5Lm0-SAhEQzwcUyg.Q8UQZUxai2WxFmoN8vJBi9cxmUUgE1Eg8EPM-P4EMzY6johjaq7fYmUVRFHnq731mQqh8URduzWBz4uJP2blL-WJJzkPYRNYKOEcs4U1sIPmqZ1Tk3vW19wPbow2RbdpBHaFA9zyL1u-c1fBDqTzRzkcG9herOzM9czE6pBxQqcA9M3NZDNbUrxxc6Q5qBWhUEmouIMEXtdMHT9SknAbmFRZH_rFlenHqRHCq2j4qTB8u4hQTzp6NpGfQWIZ1iXOpYE4CAA22CVNzk3cRdjJtm8Ia_pUJy1PLjmi7gC986xAj64Wel1lmpDsJ0dVbp7Am6SUXJCAFEgQjejEbhrS3g';
 // const OAuthToken = 'eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwCANd-rQ-zWSAgAgHUCuobs1kgCAGBu6J-HX45FpTYdVXSE3uUVAAEAAAAYAAkAAAAFAAAAKwAAAC0AAAAvAAAAMQAAADIAAAA4AAAAMwAAADUAAAANACQAAABmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgSAAEAAAALAAAAaW50ZXJhY3RpdmUwAAByFapD7NZINwD7-GeP_kubT5ICERDPBxTK.KensMUG6v-R7d1Rb_sbDYbsx244Sl4VauC1QCs9Pz6hpV3kc7saKjgfjS7u2gNP_pZzg87baDzcnjh42zDyBJIZa8SB_P2Uv3AZLW4IzMwYy9aACh7MGUGS0MH2S0Vj8rnsOEGqrJSj_Nc-SPuk7MMserTpERkz9TR9lCUN7KBmWuq6VYJoWqCC-tUSrTkd3YWHoc0L2O9yh2pFZOQBU2dp4UshmCjNBzCUpOFPgG49pwLdJwO80w4aNm5EATE8kX3PEHAj3aq8ojjOLc9zuIvtd5AS16gea4ZcAW7TvxGf8KsfhjkLp027_q-2nv0S9ZOcRH0h8b0Iwmbyr4NkfNQ'
-const OAuthToken = 'eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwAAJ5Dh9OzWSAgAAGez7zft1kgCAGBu6J-HX45FpTYdVXSE3uUVAAEAAAAYAAkAAAAFAAAAKwAAAC0AAAAvAAAAMQAAADIAAAA4AAAAMwAAADUAAAANACQAAABmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgSAAEAAAALAAAAaW50ZXJhY3RpdmUwAAD6XuD07NZINwDnlbHeHXOAS7fUyFvGfQNF.zuVkC30oi8qd9E-_g5j0I8KbxETs756FTlHu3rPVvt2Z3j5nmWe4Z75DWnLHw7oEUAKBtHqPf8mEIyCDbf9tFIghNUnbP6JbEsD4XhUcFdP7FLQjVTDv2H083cPxOvnc1alCZ5sdPkBDzRdCP3j_BHXfWBxRNY47YKNDUrCl8kbj5Kt9Y1x74E8kSHKCo_7_tORKuKGJvreexlqw0E4U8YkYYTxRvyW9_a0hX40IGrj9lODCbStHfjP-7ZPGFZnK-VBSyksJjHqF9R4iGaBDk3UJWNzmb5CgTQsHjaCX3eJjtlUs4ctY88oHrcFVB-gSeSMTIRNg30QLlByJyNEOuQ';
-
+// const OAuthToken = 'eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwAAJ5Dh9OzWSAgAAGez7zft1kgCAGBu6J-HX45FpTYdVXSE3uUVAAEAAAAYAAkAAAAFAAAAKwAAAC0AAAAvAAAAMQAAADIAAAA4AAAAMwAAADUAAAANACQAAABmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgSAAEAAAALAAAAaW50ZXJhY3RpdmUwAAD6XuD07NZINwDnlbHeHXOAS7fUyFvGfQNF.zuVkC30oi8qd9E-_g5j0I8KbxETs756FTlHu3rPVvt2Z3j5nmWe4Z75DWnLHw7oEUAKBtHqPf8mEIyCDbf9tFIghNUnbP6JbEsD4XhUcFdP7FLQjVTDv2H083cPxOvnc1alCZ5sdPkBDzRdCP3j_BHXfWBxRNY47YKNDUrCl8kbj5Kt9Y1x74E8kSHKCo_7_tORKuKGJvreexlqw0E4U8YkYYTxRvyW9_a0hX40IGrj9lODCbStHfjP-7ZPGFZnK-VBSyksJjHqF9R4iGaBDk3UJWNzmb5CgTQsHjaCX3eJjtlUs4ctY88oHrcFVB-gSeSMTIRNg30QLlByJyNEOuQ';
+const OAuthToken =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjY4MTg1ZmYxLTRlNTEtNGNlOS1hZjFjLTY4OTgxMjIwMzMxNyJ9.eyJUb2tlblR5cGUiOjUsIklzc3VlSW5zdGFudCI6MTU4MTgyMzc3OSwiZXhwIjoxNTgxODUyNTc5LCJVc2VySWQiOiI5ZmU4NmU2MC01Zjg3LTQ1OGUtYTUzNi0xZDU1NzQ4NGRlZTUiLCJzaXRlaWQiOjEsInNjcCI6WyJzaWduYXR1cmUiLCJjbGljay5tYW5hZ2UiLCJvcmdhbml6YXRpb25fcmVhZCIsInJvb21fZm9ybXMiLCJncm91cF9yZWFkIiwicGVybWlzc2lvbl9yZWFkIiwidXNlcl9yZWFkIiwidXNlcl93cml0ZSIsImFjY291bnRfcmVhZCIsImRvbWFpbl9yZWFkIiwiaWRlbnRpdHlfcHJvdmlkZXJfcmVhZCIsImR0ci5yb29tcy5yZWFkIiwiZHRyLnJvb21zLndyaXRlIiwiZHRyLmRvY3VtZW50cy5yZWFkIiwiZHRyLmRvY3VtZW50cy53cml0ZSIsImR0ci5wcm9maWxlLnJlYWQiLCJkdHIucHJvZmlsZS53cml0ZSIsImR0ci5jb21wYW55LnJlYWQiLCJkdHIuY29tcGFueS53cml0ZSJdLCJhdWQiOiJmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgiLCJhenAiOiJmMGYyN2YwZS04NTdkLTRhNzEtYTRkYS0zMmNlY2FlM2E5NzgiLCJpc3MiOiJodHRwczovL2FjY291bnQtZC5kb2N1c2lnbi5jb20vIiwic3ViIjoiOWZlODZlNjAtNWY4Ny00NThlLWE1MzYtMWQ1NTc0ODRkZWU1IiwiYW1yIjpbImludGVyYWN0aXZlIl0sImF1dGhfdGltZSI6MTU4MTgyMzc3OCwicHdpZCI6IjlkYjE1YWI1LWE2ZWMtNDhkNS1hZDM3LTczMGMzZDNjYTViZiJ9.31vrLSfZK3wp_S6Mmz1BCkhHupagI2i5Qpm-fZxMIUcTYve3zyi3Qk-jjtO1j87ZxJtrOdeueRIizeR3R6M5A726kU5VCu0U8c8S0v9AtUytEwSGZ16Ax0VlqhAv9ecTBEJIqxyoHQKmC4kAMhyHMoO2aJ1xisJXBaGc4xOg-GLGHTvekB0A2KJXrpqo-IcZvH16RlnTkG-_Q1jYzldY5cdoUXt9CPpH_j3wjtM8ObKYhhuJ4SK68G9si9kJ0PWoJSBeScDMn4sJyBXWW13S7RZIbOowSgvpHCakSQioc3I0kgQ5pIXvv1URzl9UQbz0KhKy_hdTYapVc3ImuQ6vXw';
 const accountId = 'f5afd249-305e-4cc8-a456-d16380991b61';
+// const accountId = 'f5afd249-305e-4cc8-a456-d16380991b61';
 // const accountId = '9fe86e60-5f87-458e-a536-1d557484dee5';
-
 
 //Recipient Information goes here
 const recipientName = 'mario';
 // const recipientEmail = 'yi.kan.mui@gmail.com';
-const recipientEmail = 'zzhao12@gmail.com';
+const recipientEmail = 'yi.kan.mui@gmail.com';
 
 //Point this to the document you wish to send's location on the local machine. Default location is __workingDir\fileName
 // const fileName = 'docs/House.pdf'; //IE: test.pdf
@@ -38,13 +38,11 @@ const fileName = 'docs/test2.pdf';
 //-------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------
 
-app.get('/', function (req, res) {
-
+app.get('/', function(req, res) {
   apiClient.setBasePath('https://demo.docusign.net/restapi');
   apiClient.addDefaultHeader('Authorization', 'Bearer ' + OAuthToken);
 
   // *** Begin envelope creation ***
-
 
   //Read the file you wish to send from the local machine.
   fileStream = process.argv[2];
@@ -57,7 +55,8 @@ app.get('/', function (req, res) {
 
   //Set the Email Subject line and email message
   envDef.emailSubject = 'Please sign this document sent from Node SDK';
-  envDef.emailBlurb = 'Please sign this document sent from the DocuSign Node.JS SDK.'
+  envDef.emailBlurb =
+    'Please sign this document sent from the DocuSign Node.JS SDK.';
 
   //Read the file from the document and convert it to a Base64String
   var doc = new docusign.Document();
@@ -114,21 +113,22 @@ app.get('/', function (req, res) {
 
   // *** End envelope creation ***
 
-
   //Send the envelope
   var envelopesApi = new docusign.EnvelopesApi();
-  envelopesApi.createEnvelope(accountId, { 'envelopeDefinition': envDef }, function (err, envelopeSummary, response) {
+  envelopesApi.createEnvelope(
+    accountId,
+    { envelopeDefinition: envDef },
+    function(err, envelopeSummary, response) {
+      if (err) {
+        return res.send('Error while sending a DocuSign envelope:' + err);
+      }
 
-    if (err) {
-      return res.send('Error while sending a DocuSign envelope:' + err);
+      res.send(envelopeSummary);
     }
-
-    res.send(envelopeSummary);
-
-  });
+  );
 });
 
-app.listen(port, host, function (err) {
+app.listen(port, host, function(err) {
   if (err) {
     return res.send('Error while starting the server:' + err);
   }
